6e775193dc6a13b98d4a335bd80a1c21593f2f12
nick-bennett <nick@nickbenn.com>
2022-10-18 08:43:40 -0600

    Defined constructor, stub methods, accessors.

--- src/main/java/com/tlglearning/nim/model/Pile.java
+++ src/main/java/com/tlglearning/nim/model/Pile.java
@@ -5,11 +5,33 @@ public class Pile {
   private int removed;
   private int remaining;
 
-  // TODO: 2022-10-18 Define constructor that initializes removed to 0 and sets remaining to the value of a parameter.
+  // DONE: 2022-10-18 Define constructor that initializes removed to 0 and sets remaining to the value of a parameter.
 
-  // TODO: 2022-10-18 Define methods shown in nim-classes.svg as stubs.
+  public Pile(int size) {
+    removed = 0;
+    remaining = size;
+  }
 
-  // TODO: 2022-10-18 Generate "getters" (accessors) for removed and remaining.
+  // DONE: 2022-10-18 Define methods shown in nim-classes.svg as stubs.
+
+  public int remove(int quantity) {
+    throw new UnsupportedOperationException("Not yet implemented"); // TODO: 2022-10-18 Implement remove method.
+  }
+
+  public boolean isEmpty() {
+    throw new UnsupportedOperationException("Not yet implemented"); // TODO: 2022-10-18 Implement isEmpty method.
+  }
+
+  // DONE: 2022-10-18 Generate "getters" (accessors) for removed and remaining.
+
+  public int getRemoved() {
+    return removed;
+  }
+
+  public int getRemaining() {
+    return remaining;
+  }
 
   // TODO: 2022-10-18 Create a test class for Pile.
+
 }
