f7d04a73968a56a95b6139b5703995347a6808f0
nick-bennett <nick@nickbenn.com>
2022-10-18 08:22:21 -0600

    Started implementation of Pile class.

---
+++ src/main/java/com/tlglearning/nim/model/Pile.java
@@ -0,0 +1,15 @@
+package com.tlglearning.nim.model;
+
+public class Pile {
+
+  private int removed;
+  private int remaining;
+
+  // TODO: 2022-10-18 Define constructor that initializes removed to 0 and sets remaining to the value of a parameter.
+
+  // TODO: 2022-10-18 Define methods shown in nim-classes.svg as stubs.
+
+  // TODO: 2022-10-18 Generate "getters" (accessors) for removed and remaining.
+
+  // TODO: 2022-10-18 Create a test class for Pile.
+}
