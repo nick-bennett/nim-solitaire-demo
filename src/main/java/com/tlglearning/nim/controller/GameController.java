2394d8d5ace993ce803d81e0118941b4182de1e0
nick-bennett <nick@nickbenn.com>
2022-11-09 16:37:35 -0700

    Game controller implementation in progress.

--- src/main/java/com/tlglearning/nim/controller/GameController.java
+++ src/main/java/com/tlglearning/nim/controller/GameController.java
@@ -2,16 +2,36 @@ package com.tlglearning.nim.controller;
 
 import com.tlglearning.nim.model.Game;
 import com.tlglearning.nim.model.Game.State;
+import com.tlglearning.nim.strategy.Strategy;
 import com.tlglearning.nim.view.GameView;
 
 public class GameController {
 
   private final Game game;
   private final GameView view;
+  private final Strategy strategy;
 
-  public GameController(State initialState, int... pileSizes) {
+  public GameController(State initialState, Strategy strategy, int... pileSizes) {
     game = new Game(initialState, pileSizes);
     view = new GameView();
+    this.strategy = strategy;
+  }
+
+  public State play() {
+    State state = game.getState();
+    while (!state.isTerminal()) {
+      int[] move;
+      if (state == State.PLAYER_1_MOVE) {
+        move = strategy.selectMove(game);
+      } else {
+        System.out.println(view.toString(game));
+        // TODO: 2022-11-09 Get the user's move.
+      }
+      game.play(game.getPiles().get(move[0] - 1), move[1]);
+      state = game.getState();
+      // TODO: 2022-11-09 Display the move made, by whom, and the outcome (if terminal).
+    }
+    return state;
   }
 
 }
--- src/main/java/com/tlglearning/nim/strategy/OptimalStrategy.java
+++ src/main/java/com/tlglearning/nim/strategy/OptimalStrategy.java
@@ -2,31 +2,50 @@ package com.tlglearning.nim.strategy;
 
 import com.tlglearning.nim.model.Game;
 import com.tlglearning.nim.model.Game.Pile;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class OptimalStrategy implements Strategy {
 
+  private final Random rng = new Random();
+
   @Override
   public int[] selectMove(Game game) {
-    int nimSum = game
-        .getPiles()
+    int[] move;
+    List<Pile> piles = game.getPiles();
+    int nimSum = piles
         .stream()
         .mapToInt(Pile::getRemaining)
         .reduce(0, (a, b) -> a ^ b);
     if (nimSum != 0) {
-      int[] candidateMoves = game
-          .getPiles()
-          .stream()
-          .mapToInt((pile) -> {
+      Iterator<Pile> iterator = piles.iterator();
+      int[][] candidateMoves = IntStream.rangeClosed(1, piles.size())
+          .mapToObj((pileNumber) -> {
+            int[] candidate = {pileNumber, 0};
+            Pile pile = iterator.next();
             int remaining = pile.getRemaining();
             int temp = nimSum ^ remaining;
-            return (temp < remaining) ? (remaining - temp) : 0;
+            candidate[1] = (temp < remaining) ? (remaining - temp) : 0;
+            return candidate;
           })
-          .toArray();
-      // TODO: 2022-11-08 Pick a non-zero element in candidate moves, and return an array made up
+          .filter((candidate) -> candidate[1] > 0)
+          .toArray(int[][]::new);
+      // DONE: 2022-11-08 Pick a non-zero element in candidate moves, and return an array made up
       //  the position of the element, and the value in that position of the array.
+      move = candidateMoves[rng.nextInt(candidateMoves.length)];
     } else {
-      // TODO: 2022-11-08 Pick a move at random.
+      // DONE: 2022-11-08 Pick a move at random.
+      Iterator<Pile> iterator = piles.iterator();
+      int[][] candidateMoves = IntStream.rangeClosed(1, piles.size())
+          .mapToObj((pileNumber) ->
+              new int[]{pileNumber, Math.min(iterator.next().getRemaining(), 1)})
+          .filter((candidate) -> candidate[1] > 0)
+          .toArray(int[][]::new);
+      move = candidateMoves[rng.nextInt(candidateMoves.length)];
     }
+    return move;
   }
 
 }
